536cbeead09f3d5f26b5a3e70953a3dc5f765111
Matheus-Ileck-flooxai <matheus.ileck.flooxai@gmail.com>
2025-04-16 12:05:04 -0300

    feat: add Menu component

---
+++ frontend/src/template/menu.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default props => (
+
+    <nav className='navbar navbar-inverse bg-inverse'>
+        <div className='navbar-header'>
+            <a href="" className='navbar-brand'>
+                <i className='fa fa-calendar-check-o'></i> TodoApp
+            </a>
+
+        </div>
+
+        <div id='navbar' className='navbar-collapse collapse'>
+            <ul className='nav navbar-nav'>
+                <li><a href="#/todos">Tarefas</a></li>
+                <li><a href="#/todos">Sobre</a></li>
+
+            </ul>
+
+        </div>
+
+    </nav>    
+
+)
