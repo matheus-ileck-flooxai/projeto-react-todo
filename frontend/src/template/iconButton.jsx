d56bc0082a05f5f06a4893e98055d8009ceebcad
Matheus-Ileck-flooxai <matheus.ileck.flooxai@gmail.com>
2025-04-16 12:55:48 -0300

    refactor: use conditional render with If component

--- frontend/src/template/iconButton.jsx
+++ frontend/src/template/iconButton.jsx
@@ -1,18 +1,13 @@
 import React from "react";
+import If from './if'
 
-export default props => {
+export default props => (
 
-    if (props.hide) {
-        return null
-    }
-    else {
-        return (
+    <If test={!props.hide}>
+        <button className={'btn btn-' + props.style} onClick={props.onClick}>
+            <i className={'fa fa-' + props.icon}></i>
+        </button>
+    </If>
 
-            <button className={'btn btn-' + props.style} onClick={props.onClick}>
-                <i className={'fa fa-' + props.icon}></i>
-            </button>
 
-        )
-    }
-
-}
+)
---
+++ frontend/src/template/if.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+export default props => {
+
+    if (props.test) {
+
+        return props.children
+    }
+    else {
+        return false
+    }
+
+}
