a1cfe70dfecc4f20e9b6c5de9c1673776c16691b
Matheus-Ileck-flooxai <matheus.ileck.flooxai@gmail.com>
2025-04-17 10:35:59 -0300

    feat: add keyboard shortcuts

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -2,23 +2,51 @@ import React from "react";
 import Grid from '../template/grid'
 import IconButton from "../template/iconButton";
 
-export default props => (
+export default props => {
 
-    <div role="form" className="todoForm">
+    const keyHandler = (e) => {
+        if(e.key === 'Enter') {
+
+            e.shiftKey ? props.handleSearch() : props.handleAdd()
+        }
+        else if (e.key ===  'Escape'){
+            props.handleClear()
+        }
+    }
+
+    return (
+
+        <div role="form" className="todoForm">
         <Grid cols='12 9 10'>
-            <input id="description" className="form-control"
-                placeholder="Adicione uma tarefa" value={props.description} onChange={props.handleChange} />
+            <input 
+            id="description" 
+            className="form-control"
+            placeholder="Adicione uma tarefa" 
+            value={props.description} 
+            onChange={props.handleChange} 
+            onKeyUp={keyHandler}/>
         </Grid>
         <Grid cols='12 3 2'>
             <IconButton style='primary' icon='plus' 
                 onClick={props.handleAdd}
             ></IconButton>
 
-            <IconButton style='info' icon='search' onClick={props.handleSearch}></IconButton>
+            <IconButton 
+            style='info' 
+            icon='search' 
+            onClick={props.handleSearch}>
 
-            <IconButton style='default' icon='close' onClick={props.handleClear}></IconButton>
+            </IconButton>
+
+            <IconButton 
+            style='default' 
+            icon='close' 
+            onClick={props.handleClear}>
+
+            </IconButton>
         </Grid>
     </div>
 
+    )
 
-)
+}
