a67b4825a0ed40758a8f13781cf781483789591b
Matheus-Ileck-flooxai <matheus.ileck.flooxai@gmail.com>
2025-04-23 09:19:21 -0300

    feat: add CRUD with Redux

--- frontend/src/todo/todoList.jsx
+++ frontend/src/todo/todoList.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { markAsDone, markAsPending, remove } from "./todoActions";
 import IconButton from "../template/iconButton";
 
-const TodoList =  props => {
+const TodoList = props => {
 
     const renderRows = () => {
 
@@ -13,9 +15,9 @@ const TodoList =  props => {
             <tr key={todo._id}>
                 <td className={todo.done ? 'markedAsDone' : ''}>{todo.description}</td>
                 <td>
-                    <IconButton style='success' icon='check' hide={todo.done} onClick={() => props.handleMarkAsDone(todo)}></IconButton>
-                    <IconButton style='warning' icon='undo' hide={!todo.done} onClick={() => props.handleMarkAsPending(todo)}></IconButton>
-                    <IconButton style='danger' icon='trash-o' hide={!todo.done} onClick={() => props.handleRemove(todo)}></IconButton>
+                    <IconButton style='success' icon='check' hide={todo.done} onClick={() => props.markAsDone(todo)}></IconButton>
+                    <IconButton style='warning' icon='undo' hide={!todo.done} onClick={() => props.markAsPending(todo)}></IconButton>
+                    <IconButton style='danger' icon='trash-o' hide={!todo.done} onClick={() => props.remove(todo)}></IconButton>
                 </td>
             </tr>
 
@@ -38,6 +40,7 @@ const TodoList =  props => {
     )
 }
 
-const mapStateToProps = state => ({list: state.todo.list})
+const mapStateToProps = state => ({ list: state.todo.list })
+const mapDispatchToProps = dispatch => bindActionCreators({ markAsDone, markAsPending, remove }, dispatch)
 
-export default connect(mapStateToProps)(TodoList)
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList)
